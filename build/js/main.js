68537d36858363ca494f944cb7114a2862f46a88
denis <furik1102@gmail.com>
2018-07-19 12:50:41 +0300

    all_all

--- build/js/main.js
+++ build/js/main.js
@@ -14310,12 +14310,65 @@ return jQuery;
 
 (function ($) {
     $(document).ready(function () {
+        if ($(window).width() > 1200) {
+            $('.menu-item-has-children').hover(
+                function () {
+                    $('.dropdown-list').toggleClass('active');
+                }
+            );
+        }
 
-        $('.menu-item-has-children').hover(
-            function () {
-                $('.dropdown-list').toggleClass('active');
+        //mobile menu
+
+        $('.menu-button').on('click', function() {
+
+            $(this).toggleClass('active');
+            $('.top_nav').toggleClass('active');
+            $('body').toggleClass('menu-active');
+
+
+        });
+        //form contact
+
+        $('.contact_form_open').on('click', function() {
+            $('.contact_form_wrap').toggleClass('active');
+        });
+
+        //form search
+        $('.header_btn.icon-search').on('click', function() {
+            $('.header_form').toggleClass('active');
+            return false;
+        });
+
+        if ($(window).width() < 992) {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 10) {
+                    $('.header_form').removeClass('active');
+                }
+
+            });
+        }
+
+        //language
+        $('.language-select').click(function(){
+            $(this).toggleClass('open');
+        })
+
+        $('.language-select li').click(function(){
+            $('ul li').removeClass('active');
+            $(this).toggleClass('active');
+        })
+
+        $(document).mouseup(function (e) {
+            var container = $(".language-select");
+            if (container.has(e.target).length === 0){
+                container.removeClass('open');
             }
-        );
+        });
+
+
+        //slider
+
         $('.view_slider .view_content').slick({
             arrows: true,
             autoplay: false,
@@ -14324,6 +14377,8 @@ return jQuery;
             dots: true,
             infinite: true
         });
+
+
         //counter
         $('.counter').each(function() {
             var $this = $(this),
@@ -14350,10 +14405,9 @@ return jQuery;
                         }
 
                     });
-
-
+                });
 
             });
         });
-    });
+
 })(jQuery);
